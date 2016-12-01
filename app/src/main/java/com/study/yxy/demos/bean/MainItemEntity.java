3b945a46e9ad22b4fc3471943e8d32a3abbeba31
xinyu.yang <xinyu.yang@21chinamall.com>
2016-12-01 18:19:49 +0800

    运用dataBinding初步完成主页

---
+++ app/src/main/java/com/study/yxy/demos/bean/MainItemEntity.java
@@ -0,0 +1,15 @@
+package com.study.yxy.demos.bean;
+
+/**
+ * Created by yxy on 2016/12/1.
+ */
+
+public class MainItemEntity {
+    public int id;
+    public String itemName;
+
+    public MainItemEntity(int id, String itemName) {
+        this.id = id;
+        this.itemName = itemName;
+    }
+}
