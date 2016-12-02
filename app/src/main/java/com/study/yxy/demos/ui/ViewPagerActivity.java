a156e9a28dec1a2d0c47a80c400f45cb3ae036ba
xinyu.yang <xinyu.yang@21chinamall.com>
2016-12-02 16:11:20 +0800

    viewpager+tablayout Demo

---
+++ app/src/main/java/com/study/yxy/demos/ui/ViewPagerActivity.java
@@ -0,0 +1,62 @@
+package com.study.yxy.demos.ui;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.study.yxy.demos.R;
+import com.study.yxy.demos.databinding.ActivityViewpagerBinding;
+import com.study.yxy.demos.ui.adapter.MyViewPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by yxy on 2016/12/2.
+ */
+
+public class ViewPagerActivity extends AppCompatActivity {
+    private List<View> mViewList;//页卡视图集合
+    private String[] imgUrls = {
+            "http://s1.dwstatic.com/group1/M00/EC/4E/a1afc26b5df89dceebf31cba56fc3a22.jpg",
+            "http://s1.dwstatic.com/group1/M00/85/E6/56fe8286c3360bb75b1e47786fcf005f.jpg",
+            "http://s1.dwstatic.com/group1/M00/BE/2F/c1dfd965277b98baa11d6a33a34f3ff5.jpg",
+            "http://s1.dwstatic.com/group1/M00/6B/2C/3d132dcc459bd64375bb7adaf02f4e8e.jpg",
+            "http://s1.dwstatic.com/group1/M00/FA/F0/90e69b2ec66fa6919fb5f26bc8b34697.jpg"};
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityViewpagerBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_viewpager);
+        initData();
+        TabLayout tabLayout = binding.tlTabsDemo;
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);//设置tab模式，当前为系统默认模式
+        MyViewPagerAdapter mPagerAdapter = new MyViewPagerAdapter(mViewList);
+        binding.setVpAdapter(mPagerAdapter);
+        tabLayout.setupWithViewPager(binding.vpDemo);//将TabLayout和ViewPager关联起来。
+        /*
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            tab.setCustomView(mPagerAdapter.getTabView(i));
+        }*/
+    }
+
+    private void initData() {
+        mViewList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            View view1 = View.inflate(this, R.layout.item_viewpager_demos, null);
+            ImageView imageView = (ImageView) view1.findViewById(R.id.iv_item_vp_demo);
+//            ImageView imageView = new ImageView(this);
+//            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(400, 300);
+//            imageView.setLayoutParams(params);
+            Glide.with(this).load(imgUrls[i]).into(imageView);
+            mViewList.add(view1);
+        }
+    }
+}
---
+++ app/src/main/java/com/study/yxy/demos/ui/adapter/MyViewPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.study.yxy.demos.ui.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public class MyViewPagerAdapter extends PagerAdapter {
+    private List<View> mViewList;
+
+    public MyViewPagerAdapter(List<View> mViewList) {
+        this.mViewList = mViewList;
+    }
+
+    @Override
+    public int getCount() {
+        return mViewList.size();//页卡数
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;//官方推荐写法
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(mViewList.get(position));//添加页卡
+        return mViewList.get(position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(mViewList.get(position));//删除页卡
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "title"+position;//页卡标题
+    }
+
+    /*
+    public View getTabView(int position){
+        View view = LayoutInflater.from(context).inflate(R.layout.tab_item, null);
+        TextView tv= (TextView) view.findViewById(R.id.textView);
+        tv.setText(tabTitles[position]);
+        ImageView img = (ImageView) view.findViewById(R.id.imageView);
+        img.setImageResource(imageResId[position]);
+        return view;
+    }*/
+}
