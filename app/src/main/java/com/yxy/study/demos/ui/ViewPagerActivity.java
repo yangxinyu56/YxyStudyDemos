6c0cb85a363a8c03d73edf5423942b3e583c1dcd
touch <120233386@qq.com>
2017-07-11 09:25:35 +0800

    测试databinding 双向绑定

--- app/src/main/java/com/yxy/study/demos/ui/ViewPagerActivity.java
+++ app/src/main/java/com/yxy/study/demos/ui/ViewPagerActivity.java
@@ -29,7 +29,10 @@ public class ViewPagerActivity extends AppCompatActivity {
             "http://s1.dwstatic.com/group1/M00/85/E6/56fe8286c3360bb75b1e47786fcf005f.jpg",
             "http://s1.dwstatic.com/group1/M00/BE/2F/c1dfd965277b98baa11d6a33a34f3ff5.jpg",
             "http://s1.dwstatic.com/group1/M00/6B/2C/3d132dcc459bd64375bb7adaf02f4e8e.jpg",
-            "http://s1.dwstatic.com/group1/M00/FA/F0/90e69b2ec66fa6919fb5f26bc8b34697.jpg"};
+            "http://s1.dwstatic.com/group1/M00/FA/F0/90e69b2ec66fa6919fb5f26bc8b34697.jpg",
+            "http://s1.dwstatic.com/group1/M00/EC/4E/a1afc26b5df89dceebf31cba56fc3a22.jpg",
+            "http://s1.dwstatic.com/group1/M00/85/E6/56fe8286c3360bb75b1e47786fcf005f.jpg",
+            "http://s1.dwstatic.com/group1/M00/BE/2F/c1dfd965277b98baa11d6a33a34f3ff5.jpg"};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,7 +40,7 @@ public class ViewPagerActivity extends AppCompatActivity {
         ActivityViewpagerBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_viewpager);
         initData();
         TabLayout tabLayout = binding.tlTabsDemo;
-        tabLayout.setTabMode(TabLayout.MODE_FIXED);//设置tab模式，当前为系统默认模式
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);//设置tab模式，当前为可滚动
         MyViewPagerAdapter mPagerAdapter = new MyViewPagerAdapter(mViewList);
         binding.setVpAdapter(mPagerAdapter);
         tabLayout.setupWithViewPager(binding.vpDemo);//将TabLayout和ViewPager关联起来。
@@ -50,7 +53,7 @@ public class ViewPagerActivity extends AppCompatActivity {
 
     private void initData() {
         mViewList = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 8; i++) {
             View view1 = View.inflate(this, R.layout.item_viewpager_demos, null);
             ImageView imageView = (ImageView) view1.findViewById(R.id.iv_item_vp_demo);
             final int finalI = i;
